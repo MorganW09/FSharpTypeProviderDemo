8b2fcbba2fafff1397264a1a44badc1e62b7538a
Morgan Kenyon <morganwkenyon@gmail.com>
2019-12-18 22:17:59 -0600

    Further work.

--- src/TypeDemo/Program.fs
+++ src/TypeDemo/Program.fs
@@ -1,14 +1,34 @@
 ﻿open FSharp.Data
+open System
+
+[<Literal>]
+let connectionString = 
+    "Server=localhost;Database=AdventureWorksLT2017;Trusted_Connection=True;"
+
+[<Literal>]
+let connectionString2 = 
+    "Server=localhost;Database=AdventureWorks2017;Trusted_Connection=True;"
+    
+
+type AdventureWorks2017 = SqlProgrammabilityProvider<connectionString2>
 
 type Product =
     {
-        ProductID : int
         Name : string
-        Color : string option
+        ProductNumber : string
+        Color : string
+        StandardCost : decimal
+        ListPrice : decimal
+        Size : string
+        Weight : decimal
+        ProductCategoryID : int
+        ProductModelID : int
+        SellStartDate : DateTime
+        SellEndDate : DateTime
+        DiscontinuedDate : DateTime
+        Rowguid : Guid
+        ModifiedDate : DateTime
     }
-[<Literal>]
-let connectionString = 
-    @"Server=localhost;Database=AdventureWorksLT2017;Trusted_Connection=True;"
 
 let getProducts () = 
     let cmd = new SqlCommandProvider<"
@@ -20,7 +40,7 @@ let getProducts () =
     
     cmd.Execute()
 
-let getRedProducts () =
+let getRedProducts (color) =
     let cmd = new SqlCommandProvider<"
         SELECT TOP (1000) 
         [ProductID]
@@ -29,17 +49,55 @@ let getRedProducts () =
         FROM [SalesLT].[Product]
         WHERE Color = @color", connectionString>(connectionString)
 
-    cmd.Execute("Red");
+    cmd.Execute(color)
 
+let insertProduct (product : Product) =
+    let cmd = new SqlCommandProvider<"
+    INSERT INTO [SalesLT].[Product] 
+        (Name, ProductNumber, Color, StandardCost, 
+        ListPrice, Size, Weight, ProductCategoryID, ProductModelID, SellStartDate, 
+        SellEndDate, DiscontinuedDate, rowguid, ModifiedDate)
+    VALUES (@Name, @ProductNumber, @Color, @StandardCost, @ListPrice, @Size, 
+        @Weight, @ProductCategoryID, @ProductModelID, @SellStartDate, @SellEndDate,
+        @DistinuedDate, @rowguid, @ModifiedDate)", connectionString>(connectionString)
+
+    cmd.Execute(product.Name, product.ProductNumber, product.Color, product.StandardCost, product.ListPrice, 
+        product.Size, product.Weight, product.ProductCategoryID, product.ProductModelID, product.SellStartDate, 
+        product.SellEndDate, product.DiscontinuedDate, product.Rowguid, product.ModifiedDate)
+
+let deleteProduct (productNumber) =
+    let cmd = new SqlCommandProvider<"
+        DELETE FROM [SalesLT].[Product]
+        WHERE ProductNumber = @ProductNumber", connectionString>(connectionString)
+
+    cmd.Execute(productNumber)
+
+//Make sure you return a list or array
+//https://github.com/fsprojects/FSharp.Data.SqlClient/issues/321
+let getEmployeeManagers (businesEntityID : int) =
+    use cmd = new AdventureWorks2017.dbo.uspGetEmployeeManagers(connectionString2)
+    cmd.Execute(businesEntityID) |> Seq.toList
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
     let records = getProducts()
     records |> printfn "%A"
     let product722 = records |> Seq.filter (fun x -> x.ProductID = 722)
     printfn "record 722 %A" product722
     
-    let redRecords = getRedProducts()
+    let redRecords = getRedProducts("Red")
     redRecords |> printfn "%A"
-    Seq.length redRecords |> printfn "number of red records %d" 
+    Seq.length redRecords |> printfn "number of red records %d"
+    
+    let newProduct = { Name = "Touring Double Rear Wheel"; ProductNumber = "RW-T905a"; Color = "Red"; 
+        StandardCost = 200.82m; ListPrice = 400.1m; Size = "40"; Weight = 1200.44m; ProductCategoryID = 21; 
+        ProductModelID = 14; SellStartDate = DateTime.UtcNow; SellEndDate = DateTime.MaxValue; 
+        DiscontinuedDate = DateTime.MaxValue; Rowguid = Guid.NewGuid(); ModifiedDate = DateTime.UtcNow }
+    let insertedCount = insertProduct(newProduct)
+    printfn "Products inserted: %i" insertedCount
+
+    let deletedCount = deleteProduct("RW-T905a")
+    printfn "Products deleted: %i" deletedCount
+
+    let employeeManagers = getEmployeeManagers(120)
+    printfn "Employee Managers: %A" employeeManagers
     0
